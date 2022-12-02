0bb99f5bd94239b74cf3d1a3fe7cc61876dbf12c
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-12-02 14:10:00 +0800

    Resynthesized
    Resynthesized

--- MIPS35_Dynamic_Pipeline.runs/impl_1/top.tcl
+++ MIPS35_Dynamic_Pipeline.runs/impl_1/top.tcl
@@ -48,95 +48,12 @@ set_msg_config -id {HDL 9-1654} -limit 100000
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 
-start_step init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param xicom.use_bs_reader 1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.cache/wt [current_project]
-  set_property parent.project_path D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.xpr [current_project]
-  set_property ip_repo_paths d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.cache/ip [current_project]
-  set_property ip_output_repo d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.cache/ip [current_project]
-  set_property XPM_LIBRARIES XPM_MEMORY [current_project]
-  add_files -quiet D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.runs/synth_1/top.dcp
-  add_files -quiet d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0.dcp
-  set_property netlist_only true [get_files d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0.dcp]
-  add_files -quiet D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/ila_0/ila_0.dcp
-  set_property netlist_only true [get_files D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/ila_0/ila_0.dcp]
-  read_xdc -mode out_of_context -ref dist_mem_gen_0 -cells U0 d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0_ooc.xdc
-  set_property processing_order EARLY [get_files d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0_ooc.xdc]
-  read_xdc -mode out_of_context -ref ila_0 -cells inst d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/ila_0/ila_0_ooc.xdc
-  set_property processing_order EARLY [get_files d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/ila_0/ila_0_ooc.xdc]
-  read_xdc -ref ila_0 -cells inst d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/ila_0/ila_v6_1/constraints/ila.xdc
-  set_property processing_order EARLY [get_files d:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/sources_1/ip/ila_0/ila_v6_1/constraints/ila.xdc]
-  read_xdc D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.srcs/constrs_1/new/tb.xdc
-  link_design -top top -part xc7a100tcsg324-1
-  write_hwdef -file top.hwdef
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-}
-
-start_step opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force top_opt.dcp
-  report_drc -file top_drc_opted.rpt
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-}
-
-start_step place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  implement_debug_core 
-  place_design 
-  write_checkpoint -force top_placed.dcp
-  report_io -file top_io_placed.rpt
-  report_utilization -file top_utilization_placed.rpt -pb top_utilization_placed.pb
-  report_control_sets -verbose -file top_control_sets_placed.rpt
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-}
-
-start_step route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force top_routed.dcp
-  report_drc -file top_drc_routed.rpt -pb top_drc_routed.pb
-  report_timing_summary -warn_on_violation -max_paths 10 -file top_timing_summary_routed.rpt -rpx top_timing_summary_routed.rpx
-  report_power -file top_power_routed.rpt -pb top_power_summary_routed.pb -rpx top_power_routed.rpx
-  report_route_status -file top_route_status.rpt -pb top_route_status.pb
-  report_clock_utilization -file top_clock_utilization_routed.rpt
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-}
-
 start_step write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param xicom.use_bs_reader 1
+  open_checkpoint top_routed.dcp
+  set_property webtalk.parent_dir D:/Projects/Vivado/MIPS35_Dynamic_Pipeline/MIPS35_Dynamic_Pipeline.cache/wt [current_project]
   catch { write_mem_info -force top.mmi }
   write_bitstream -force top.bit 
   catch { write_sysdef -hwdef top.hwdef -bitfile top.bit -meminfo top.mmi -file top.sysdef }
